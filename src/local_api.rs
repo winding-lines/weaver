940172eb8bceb8d6558d3bde0f7a7d7995a2040f
Marius Seritan <git@winding-lines.com>
2018-05-13 07:08:04 -0700

    Introduce dispatch macro
    DRY the local-api code by using a dispatch macro.

--- src/local_api.rs
+++ src/local_api.rs
@@ -1,51 +1,59 @@
 //! Implement the API to be used on the client. By default it will either use the server
 //! but can also use the local database, if available.
 
-use weaver_db::{actions2, epics, Destination};
-use weaver_db::entities::{FormattedAction, NewAction};
+use weaver_db::{actions2, Destination, epics};
 use weaver_db::config::Environment;
-use weaver_error::{Result};
+use weaver_db::entities::{FormattedAction, NewAction};
+use weaver_error::Result;
 use weaver_rpc::client;
 
+// Macro to dispatch to local or remote api
+//
+// It takes 5 arguments:
+//   - destination
+//   - name (with optional module) of the local function
+//   - additional arguments for the local function, the first will be the database connection
+//   - name (with optional module) of the remote function
+//   - additional arguments for the remote function, the first one will be the remote address
+
+macro_rules! dispatch {
+    ($dest:expr, $local:path, ($($larg:tt)*), $remote:path, ($($rarg:tt)*) ) => ({
+        match *$dest {
+            Destination::Local(Ok(ref connection)) =>
+                $local(connection, $($larg)*),
+            Destination::Local(Err(_)) => Err("bad connection".into()),
+            Destination::Remote(ref rpc_addr) =>
+                $remote(rpc_addr, $($rarg)* )
+        }
+    })
+}
 
 pub fn history(env: &Environment, destination: &Destination) -> Result<Vec<FormattedAction>> {
-    match *destination {
-        Destination::Local(Ok(ref connection)) =>
-            actions2::fetch_all(connection),
-        Destination::Local(Err(_)) => Err("bad connection".into()),
-        Destination::Remote(ref rpc_addr) =>
-            client::history(env, rpc_addr)
-    }
+    dispatch!(destination,
+        actions2::fetch_all, (),
+        client::history, (env))
 }
 
 // Return the last url action from the store.
 pub fn last_url(destination: &Destination) -> Result<Option<(String, String)>> {
-    match *destination {
-        Destination::Local(Ok(ref connection)) =>
-            actions2::last_url(connection),
-        Destination::Local(Err(_)) => Err("bad connection".into()),
-        Destination::Remote(ref rpc_addr) =>
-            client::last_url(rpc_addr)
-    }
+    dispatch!( destination,
+            actions2::last_url, (),
+            client::last_url, ()
+    )
 }
 
 pub fn insert_action(new_action: NewAction, destination: &Destination) -> Result<u64> {
-    match *destination {
-        Destination::Local(Ok(ref connection)) =>
-            actions2::insert(connection, new_action),
-        Destination::Local(Err(_)) => Err("bad connection".into()),
-        Destination::Remote(ref rpc_addr) =>
-            client::add(new_action, rpc_addr)
-    }
+    dispatch!(destination,
+            actions2::insert, (new_action),
+            client::add, (new_action)
+    )
 }
 
 pub fn epic_names(destination: &Destination) -> Result<Vec<String>> {
-    match *destination {
-        Destination::Local(Ok(ref connection)) =>
-            epics::fetch_all(&connection),
-        Destination::Local(Err(_)) => Err("bad connection".into()),
-        Destination::Remote(ref rpc_addr) =>
-            client::fetch_epics(rpc_addr)
-    }
+    dispatch!(destination,
+            epics::fetch_all, (),
+            client::fetch_epics, ()
+    )
+}
 }
 
