09b6f9b37c78c8da57aade0adae803c1527f44da
Marius Seritan <git@winding-lines.com>
2018-06-09 20:58:26 -0700

    Add logo to the website

--- weaver-web/src/handlers/static_assets.rs
+++ weaver-web/src/handlers/static_assets.rs
@@ -1,11 +1,12 @@
+#![cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value))]
 use actix_web::{App, HttpRequest, HttpResponse, Result};
 use actix_web::http::StatusCode;
 use app_state::AppState;
 
 const FAVICON: &[u8] = include_bytes!("../../assets/favicon.ico");
+const SVGS: &[u8] = include_bytes!("../../assets/inline.svg");
 
 
-#[cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value))]
 /// favicon handler
 fn favicon(_req: HttpRequest<AppState>) -> Result<HttpResponse> {
     Ok(HttpResponse::build(StatusCode::OK)
@@ -13,8 +14,17 @@ fn favicon(_req: HttpRequest<AppState>) -> Result<HttpResponse> {
         .body(FAVICON))
 }
 
+fn svgs(_req: HttpRequest<AppState>) -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("image/svg+xml")
+        .body(SVGS))
+}
+
 pub(crate) fn config(app: App<AppState>) -> App<AppState> {
-    app.resource("/favicon.ico", |r|
+    let app = app.resource("/favicon.ico", |r|
         r.f(favicon),
+    );
+    app.resource("/assets/inline.svg", |r|
+        r.f(svgs),
     )
 }
