fe51432a131b2da12c0a5f82c79ac90cb9b212a9
Marius Seritan <git@winding-lines.com>
2018-09-13 20:57:43 -0700

    [weaver-hud] make links clickable

--- weaver-hud/src/main.rs
+++ weaver-hud/src/main.rs
@@ -1,15 +1,30 @@
-#[cfg(osx)]
+#[cfg(target_os="macos")]
 extern crate web_view;
+extern crate serde_json;
+use serde_json as json;
+use std::process::Command;
 
 
-#[cfg(osx)]
+#[cfg(target_os="macos")]
 fn main() {
     use web_view::*;
     let size = (300, 300);
     let resizable = true;
     let debug = true;
     let init_cb = |_webview| {};
-    let frontend_cb = |_webview: &mut _, _arg: &_, _userdata: &mut _| {};
+    let frontend_cb = |_webview: &mut _, arg: &str, _userdata: &mut _| {
+        if let Ok(v) = json::from_str::<json::Value>(arg) {
+            match v["href"] {
+                json::Value::String(ref s) => {
+                    Command::new("/bin/sh")
+                    .arg("-c")
+                    .arg(format!("open {}", s))
+                    .status().expect("running open");
+                },
+                _ => {}
+            }
+        }
+    };
     let userdata = ();
     run(
         "Weaver",
@@ -23,7 +38,7 @@ fn main() {
     );
 }
 
-#[cfg(not(osxa))]
+#[cfg(not(target_os="macos"))]
 fn main() {
   println!("not supported");
 }
