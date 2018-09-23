ce80d4045d0b23fb106e6efd7f497f41f0a813f0
Marius Seritan <github@winding-lines.com>
2018-09-22 20:51:17 -0700

    [ci] fix build script error.

--- lib-server/build.rs
+++ lib-server/build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 use std::env;
 
 fn main() {
-    let shell = env::var("SHELL").unwrap_or("/bin/sh");
+    let shell = env::var("SHELL").unwrap_or(String::from("/bin/sh"));
     Command::new(shell)
         .current_dir("web")
         .arg("./build.sh")
